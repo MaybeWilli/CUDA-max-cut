b9fcddfdd8342144293c6d7d81a2cb6b7b48de6a
William Fan <wyf132@gmail.com>
2026-06-14 21:45:54 -0400

    Added test script

---
+++ test_script.sh
@@ -0,0 +1,19 @@
+./max_cut --nodes 10000 --graph powerlaw
+
+./max_cut --nodes 20000 --graph powerlaw
+
+./max_cut --nodes 50000 --graph powerlaw
+
+
+./max_cut --nodes 10000 --graph sparse
+
+./max_cut --nodes 20000 --graph sparse
+
+./max_cut --nodes 50000 --graph sparse
+
+
+./max_cut --nodes 10201 --graph grid
+
+./max_cut --nodes 20449 --graph grid
+
+./max_cut --nodes 50625 --graph grid
